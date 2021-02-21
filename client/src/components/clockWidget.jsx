212d7775cf0b28a1454effbd210f1830808a8515
Krista Singleton <kris.singlet231@gmail.com>
2021-02-20 22:11:44 -0700

    Built skeletons of Quote and Clock components

---
+++ client/src/components/clockWidget.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import classes from '../css/styles.css';
+
+const ClockWidget = () => {
+  console.log('Clock Widget fired');
+  return (
+    <div className={classes.ClockWidget}>
+      <div>Clock Widget</div>
+    </div>
+  );
+};
+
+export default ClockWidget;
