c0009546601539d118477569f1f5cb57e1724767
Krista Singleton <kris.singlet231@gmail.com>
2021-02-20 21:13:21 -0700

    Built out basic boilerplate for app.jsx file

---
+++ client/src/components/app.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      color: 'blue',
+    };
+  }
+
+  render() {
+    return (
+      <div>Ollo?</div>
+    );
+  }
+}
+
+export default App;
