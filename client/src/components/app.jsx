fa42871d9f6f7122318150571a2ac64fd3615e27
Krista Singleton <kris.singlet231@gmail.com>
2021-02-21 18:16:54 -0700

    Got background image coming in. Just need to remove annoying white border

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -1,21 +1,46 @@
 import React from 'react';
+import axios from 'axios';
 import classes from '../css/styles.css';
 import ClockWidget from './widgets/clockWidget.jsx';
 import QuoteWidget from './widgets/quoteWidget.jsx';
 import WeatherWidget from './widgets/weatherWidget.jsx';
-import backgroundUrlGenerator from './backgroundGen.jsx';
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
       message: 'Inspire',
+      backgroundImgUrl: '',
     };
+    this.backgroundUrlGenerator = this.backgroundUrlGenerator.bind(this);
+  }
+
+  componentDidMount() {
+    this.backgroundUrlGenerator();
+  }
+
+  backgroundUrlGenerator() {
+    let url;
+    axios.get('http://www.splashbase.co/api/v1/images/random')
+      .then((response) => {
+        url = response.data.url;
+        this.setState({ backgroundImgUrl: url });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }
 
   render() {
+    console.log(this.state.backgroundImgUrl);
+    const backgroundStyle = {
+      backgroundImage: `url(${this.state.backgroundImgUrl})`,
+      backgroundSize: 'cover',
+      height: '100vh',
+      margin: '0%',
+    };
     return (
-      <div>
+      <div style={backgroundStyle}>
         <div className={classes.header}>
           <h1>{this.state.message}</h1>
         </div>
@@ -27,7 +52,7 @@ class App extends React.Component {
           <ClockWidget />
         </div>
         <div>
-          <button type="button">New Background?</button>
+          <button type="button" onClick={this.backgroundUrlGenerator}>New Background?</button>
         </div>
       </div>
     );
